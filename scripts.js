d95d7a242e26d18e3a51954ede38d8e45b19dfb7
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-06 15:41:53 -0700

    update the app and ill sole the bugs

--- scripts.js
+++ scripts.js
@@ -17,13 +17,47 @@ function wordCounter(text) {
 // wordCounter() function omitted for brevity.
 
 function numberOfOccurrencesInText(word, text) {
-
-    const textArray = word.split(" ");
+    const textArray = text.split(" ");
     let wordCount = 0;
     textArray.forEach(function(element) {
-      if (word.toLowerCase() === element.toLowerCase().includes(word.toLowerCase())) {
-        wordCount++
+      if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++;
       }
     });
     return wordCount;
   }
+  
+
+// 1. create a function that will take a prashe
+// 2. store its words in a array
+// 3.run a loop over the array to check for the bad
+//   words
+// 4.replace the varibles w the bad words with other  
+//   words we decide
+// 5.display the new words in its new context
+
+
+let offensiveWords = ["zoink", "muppeteer", "biffaroni", "loopdaloop"];
+
+let sentense = "I love piclles zoink in my sandwich";
+
+function replaceOffensiveWords() {
+    let arraySentense = sentense.split(' ');
+
+    console.log("arraySentanse: " + arraySentense);
+
+    arraySentense.forEach(function(element) {
+        if (offensiveWords.includes(element)) {
+            // let offensiveWordIndex = offensiveWords.indexOf(element);
+            let arrayWordIndex = arraySentense.indexOf(element);
+            arraySentense[arrayWordIndex]="flowers";
+
+            console.log('no no no');
+          }
+    });
+    return arraySentense.join(" ");
+
+      
+    
+        
+};
